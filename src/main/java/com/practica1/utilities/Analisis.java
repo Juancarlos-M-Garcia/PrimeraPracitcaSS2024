fbe954ce03d7b0a724375f5e76d3556c6bd24bea
Manuel Garcia <juancarlosgarcia201931521@cunoc.edu.gt>
2024-08-18 15:39:57 -0600

    [FIX] funciones para el analizador

--- src/main/java/com/practica1/utilities/Analisis.java
+++ src/main/java/com/practica1/utilities/Analisis.java
@@ -1,14 +1,69 @@
-package com.practica1.objects;
+package com.practica1.utilities;
+
+
+import com.practica1.emuns.token.*;
+import com.practica1.objects.Figure;
+import com.practica1.objects.*;
 
 public class Analisis {
-    StringBuilder data;
 
-    public Analisis(){
 
+    public static double operar (Actions act, String exp1, String exp2, String lex){
+        try{
+            double number1 = Double.parseDouble(exp1);
+            double number2 = Double.parseDouble(exp2);
+            double resultado = 0 ;
+            switch(act){
+                case LESS:
+
+                   resultado = number1 - number2;
+                    break;
+                case ADD:
+                    resultado = number1 + number2;
+                    break;
+                case MULTI:
+                    resultado = number1 * number2;
+                    break;
+                case DIV:
+                    resultado = number1 / number2;
+                    break;
+            }
+
+//            ReportData.addOperation(act, line, column, lex);
+//            ReportData.addOperation(act, 0, 0, lex);
+            System.out.println("Se realizo la siguiente operacion:"+lex);
+            System.out.println("Resultado:"+ resultado);
+            return resultado;
+        }catch(NumberFormatException e){
+            return 0;
+        }
     }
 
 
-//    public analizar(String texto){
+    public static Figure CreateFigure (Figures fig, String name ,double posx, double posy, double side, Colors col){
+        switch (fig){
+            case CIRCULO:
+                return new Circle(name, side, posx, posy, col);
+            case CUADRADO:
+                return new Square(name, side,posx, posy, col);
+        }
+        return null;
+    }
 
-//    }
+
+    public static Figure CreateFigure (Figures fig, String name ,double posx, double posy, double width, double height,  Colors col){
+        switch (fig){
+            case RECTANGULO:
+                return new Rectangle(name, width, height, posx, posy, col);
+            case CUADRADO:
+                return new Line(name, posx, posy, width, height, col);
+        }
+        return null;
+    }
+
+    public static Figure CreateFigure (String name ,double posx, double posy, int sides, double width, double height,  Colors col){
+        return new Polygon(name, width, height, sides, posx, posy, col);
+    }
+
+
 }
